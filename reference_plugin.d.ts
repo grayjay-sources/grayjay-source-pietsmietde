eb61f04c85cbc2356c9e916e8e399b54ab763f97
Bluscream <Bluscream@users.noreply.github.com>
2024-04-04 19:57:26 +0200

    update repo (works uwu)

---
+++ reference_plugin.d.ts
@@ -0,0 +1,441 @@
+//Reference Scriptfile
+//Intended exclusively for auto-complete in your IDE, not for execution
+
+declare class ScriptException extends Error {
+    //If only one parameter is provided, acts as msg
+    constructor(type: string, msg: string);
+}
+
+declare class LoginRequiredException extends ScriptException {
+    constructor(msg: string);
+}
+//Alias
+declare class ScriptLoginRequiredException extends ScriptException {
+    constructor(msg: string);
+}
+
+declare class CaptchaRequiredException extends ScriptException {
+    constructor(url: string, body: string);
+}
+
+declare class CriticalException extends ScriptException {
+    constructor(msg: string);
+}
+
+declare class UnavailableException extends ScriptException {
+    constructor(msg: string);
+}
+
+declare class AgeException extends ScriptException {
+    constructor(msg: string);
+}
+
+declare class TimeoutException extends ScriptException {
+    constructor(msg: string);
+}
+
+declare class ScriptImplementationException extends ScriptException {
+    constructor(msg: string);
+}
+
+declare class Thumbnails {
+    constructor(thumbnails: Thumbnail[])
+}
+declare class Thumbnail {
+    constructor(url, quality) {
+        this.url = url ?? ""; //string
+        this.quality = quality ?? 0; //integer
+    }
+}
+
+declare class PlatformID {
+    constructor(platform: string, id: string, pluginId: string, claimType: int = 0, claimFieldType: integer = -1);
+}
+
+declare class ResultCapabilities {
+    constructor(types: string[], sorts: string[], filters: FilterGroup[])
+}
+declare class FilterGroup {
+    constructor(name: string, filters: string[], isMultiSelect: boolean, id: string);
+}
+declare class FilterCapability {
+    constructor(name: string, value: string, id: string);
+}
+
+
+declare class PlatformAuthorLink {
+    constructor(id: PlatformID, name: string, url: string, thumbnail: string, subscribers: integer?, membershipUrl: string?);
+}
+
+declare class PlatformAuthorMembershipLink {
+    constructor(id: PlatformID, name: string, url: string, thumbnail: string, subscribers: integer?, membershipUrl: string?);
+}
+
+declare interface PlatformContentDef {
+    id: PlatformID,
+    name: string,
+    thumbnails: Thumbnails,
+    author: PlatformAuthorLink,
+    datetime: integer,
+    url: string
+}
+declare interface PlatformContent {}
+
+declare interface PlatformNestedMediaContentDef extends PlatformContentDef {
+    contentUrl: string,
+    contentName: string?,
+    contentDescription: string?,
+    contentProvider: string?,
+    contentThumbnails: Thumbnails
+}
+declare class PlatformNestedMediaContent {
+    constructor(obj: PlatformNestedMediaContentDef);
+}
+
+declare interface PlatformLockedContentDef extends PlatformContentDef {
+    contentName: string?,
+    contentThumbnails: Thumbnails?,
+    unlockUrl: string,
+    lockDescription: string?,
+}
+declare class PlatformLockedContent {
+    constructor(obj: PlatformLockedContentDef);
+}
+
+
+declare interface PlatformVideoDef extends PlatformContentDef {
+    thumbnails: Thumbnails,
+    author: PlatformAuthorLink,
+
+    duration: int,
+    viewCount: long,
+    isLive: boolean,
+    shareUrl: string?
+}
+declare class PlatformVideo implements PlatformContent {
+    constructor(obj: PlatformVideoDef);
+}
+
+
+declare interface PlatformVideoDetailsDef extends PlatformVideoDef {
+    description: string,
+    video: VideoSourceDescriptor,
+    live: IVideoSource,
+    rating: IRating,
+    subtitles: SubtitleSource[]
+}
+declare class PlatformVideoDetails extends PlatformVideo {
+    constructor(obj: PlatformVideoDetailsDef);
+}
+
+declare interface PlatformPostDef extends PlatformContentDef {
+    thumbnails: string[],
+    images: string[],
+    description: string
+}
+declare class PlatformPost extends PlatformContent {
+    constructor(obj: PlatformPostDef)
+}
+
+declare interface PlatformPostDetailsDef extends PlatformPostDef {
+    rating: IRating,
+    textType: int,
+    content: String
+}
+declare class PlatformPostDetails extends PlatformPost {
+    constructor(obj: PlatformPostDetailsDef);
+}
+
+
+//Sources
+declare interface IVideoSourceDescriptor {}
+
+declare interface MuxVideoSourceDescriptorDef {
+    isUnMuxed: boolean,
+    videoSources: VideoSource[]
+}
+declare class VideoSourceDescriptor implements IVideoSourceDescriptor {
+    constructor(videoSourcesOrObj: VideoSource[]);
+}
+
+declare interface UnMuxVideoSourceDescriptorDef {
+    isUnMuxed: boolean,
+    videoSources: VideoSource[]
+}
+class UnMuxVideoSourceDescriptor implements IVideoSourceDescriptor {
+    constructor(videoSourcesOrObj: VideoSource[], audioSources: AudioSource[]);
+    constructor(videoSourcesOrObj: UnMuxVideoSourceDescriptorDef);
+}
+
+declare interface IVideoSource {
+
+}
+declare interface IAudioSource {
+
+}
+declare interface VideoUrlSourceDef implements IVideoSource {
+    width: integer,
+    height: integer,
+    container: string,
+    codec: string,
+    name: string,
+    bitrate: integer,
+    duration: integer,
+    url: string
+}
+declare class VideoUrlSource {
+    constructor(obj: VideoUrlSourceDef);
+
+    getRequestModifier(): RequestModifier?;
+}
+declare interface VideoUrlRangeSourceDef extends VideoUrlSource {
+    itagId: integer,
+    initStart: integer,
+    initEnd: integer,
+    indexStart: integer,
+    indexEnd: integer,
+}
+declare class VideoUrlRangeSource extends VideoUrlSource {
+    constructor(obj: YTVideoSourceDef);
+}
+declare interface AudioUrlSourceDef {
+    name: string,
+    bitrate: integer,
+    container: string,
+    codecs: string,
+    duration: integer,
+    url: string,
+    language: string
+}
+declare class AudioUrlSource implements IAudioSource {
+    constructor(obj: AudioUrlSourceDef);
+
+    getRequestModifier(): RequestModifier?;
+}
+declare interface AudioUrlRangeSourceDef extends AudioUrlSource {
+    itagId: integer,
+    initStart: integer,
+    initEnd: integer,
+    indexStart: integer,
+    indexEnd: integer,
+    audioChannels: integer
+}
+declare class AudioUrlRangeSource extends AudioUrlSource {
+    constructor(obj: AudioUrlRangeSourceDef);
+}
+declare interface HLSSourceDef {
+    name: string,
+    duration: integer,
+    url: string,
+    priority: boolean?,
+    language: string?
+}
+declare class HLSSource implements IVideoSource {
+    constructor(obj: HLSSourceDef);
+}
+declare interface DashSourceDef {
+    name: string,
+    duration: integer,
+    url: string,
+    language: string?
+}
+declare class DashSource implements IVideoSource {
+    constructor(obj: DashSourceDef)
+}
+
+declare interface IRequest {
+    url: string,
+    headers: Map<string, string>
+}
+declare interface IRequestModifierDef {
+    allowByteSkip: boolean
+}
+declare class RequestModifier {
+    constructor(obj: IRequestModifierDef) { }
+
+    modifyRequest(url: string, headers: Map<string, string>): IRequest;
+}
+
+//Channel
+declare interface PlatformChannelDef {
+    id: PlatformID,
+    name: string,
+    thumbnail: string,
+    banner: string,
+    subscribers: integer,
+    description: string,
+    url: string,
+    urlAlternatives: string[],
+    links: Map<string>?
+}
+declare class PlatformChannel {
+    constructor(obj: PlatformChannelDef);
+}
+
+//Playlist
+declare interface PlatformPlaylistDef implements PlatformContent {
+    videoCount: integer,
+    thumbnail: string
+}
+declare class PlatformPlaylist extends PlatformContent {
+    constructor(obj: PlatformPlaylistDef);
+}
+declare interface PlatformPlaylistDetailsDef implements PlatformPlaylistDef {
+    contents: ContentPager
+}
+declare class PlatformPlaylistDetails extends PlatformContent {
+    constructor(obj: PlatformPlaylistDetailsDef);
+}
+
+
+//Ratings
+interface IRating {
+    type: integer
+}
+declare class RatingLikes implements IRating {
+    constructor(likes: integer);
+}
+declare class RatingLikesDislikes implements IRating {
+    constructor(likes: integer, dislikes: integer);
+}
+declare class RatingScaler implements IRating {
+    constructor(value: double);
+}
+
+declare interface CommentDef {
+    contextUrl: string,
+    author: PlatformAuthorLink,
+    message: string,
+    rating: IRating,
+    date: long,
+    replyCount: int,
+    context: any
+}
+declare class PlatformComment {
+    constructor(obj: CommentDef);
+}
+
+declare class PlaybackTracker {
+    constructor(interval: integer);
+
+    setProgress(seconds: integer);
+}
+
+declare class LiveEventPager {
+    nextRequest = 4000;
+
+    constructor(results: LiveEvent[], hasMore: boolean, context: any);
+
+    hasMorePagers(): boolean
+    nextPage(): LiveEventPager; //Could be self
+}
+
+declare class LiveEvent {
+    constructor(type: integer);
+}
+declare class LiveEventComment extends LiveEvent {
+    constructor(name: string, message: string, thumbnail: string?, colorName: string?, badges: string[]);
+}
+declare class LiveEventEmojis extends LiveEvent {
+    constructor(name: Map<string>);
+}
+declare class LiveEventDonation extends LiveEvent  {
+    constructor(amount: integer, name: string, message: string, thumbnail: string?, expire: Int, colorDonation: string?);
+}
+declare class LiveEventViewCount extends LiveEvent {
+    constructor(viewCount: integer);
+}
+declare class LiveEventRaid extends LiveEvent {
+    constructor(targetUrl: string, targetName: string, targetThumbnail: string);
+}
+
+
+
+//Pagers
+declare class ContentPager {
+    constructor(results: PlatformContent[], hasMore: boolean);
+
+    hasMorePagers(): boolean
+    nextPage(): ContentPager?; //Could be self
+}
+declare class VideoPager {
+    constructor(results: PlatformVideo[], hasMore: boolean);
+
+    hasMorePagers(): boolean
+    nextPage(): VideoPager?; //Could be self
+}
+declare class ChannelPager {
+    constructor(results: PlatformChannel[], hasMore: boolean);
+
+    hasMorePagers(): boolean;
+    nextPage(): ChannelPager?; //Could be self
+}
+declare class PlaylistPager {
+    constructor(results: PlatformPlaylist[], hasMore: boolean);
+
+    hasMorePagers(): boolean;
+    nextPage(): PlaylistPager?;
+}
+declare class CommentPager {
+    constructor(results: PlatformComment[], hasMore: boolean);
+
+    hasMorePagers(): boolean
+    nextPage(): CommentPager?; //Could be self
+}
+
+interface Map<T> {
+    [Key: string]: T;
+}
+
+//To override by plugin
+
+interface Source {
+    getHome(): VideoPager;
+
+    enable(config: SourceConfig, settings: Any, savedState: string?);
+    disable();
+
+    saveState(): string;
+
+    searchSuggestions(query: string): string[];
+    search(query: string, type: string, order: string, filters): ContentPager;
+    getSearchCapabilities(): ResultCapabilities
+
+    //Optional
+    searchChannelContents(channelUrl: string, query: string, type: string, order: string, filters): ContentPager;
+    //Optional
+    getSearchChannelContentsCapabilities(): ResultCapabilities;
+
+    //Optional
+    getChannelUrlByClaim(claimType: int, values: Map<Int, String>)
+
+    isChannelUrl(url: string): boolean;
+    getChannel(url: string): PlatformChannel;
+
+    getChannelContents(url: string, type: string, order: string, filters): ContentPager;
+    getChannelCapabilities(): ResultCapabilities;
+
+    isContentDetailsUrl(url: string): boolean;
+    getContentDetails(url: string): PlatformContentDetails;
+
+    //Optional
+    getLiveEvents(url: string): LiveEventPager;
+
+    //Optional
+    getComments(url: string): CommentPager;
+    //Optional
+    getSubComments(comment: PlatformComment): CommentPager;
+
+    //Optional
+    getUserSubscriptions(): string[];
+    //Optional
+    getUserPlaylists(): string[];
+
+    //Optional
+    isPlaylistUrl(url: string): boolean;
+    //Optional
+    getPlaylist(url): string[];
+}
+
+const source: Source;
+
--- script.js
+++ script.js
@@ -1,15 +1,102 @@
 const PLATFORM = "PietsmietDE";
 
+const LIMIT_VIDEOS = 500;
+const LIMIT_COMMENTS = 100;
+
+const URL_BASE = "https://www.pietsmiet.de";
+const URL_HOME = `${URL_BASE}/api/v1/videos?limit=${LIMIT_VIDEOS}&order=latest&prioritize_featured=0&page=`;
+const URL_VIDEO_DETAILS = `${URL_BASE}/api/v1/videos/`; // include[]=playlist
+const URL_VIDEO_PLAYER = `${URL_BASE}/api/v1/utility/player?preset=quality&video=`;
+const URL_CHANNEL = `${URL_BASE}/videos/channels/`;
+const URL_PROFILE = `${URL_BASE}/profile/`
+const URL_COMMENTS = `${URL_BASE}/api/v1/utility/comments?order=popular&type=video&limit=${LIMIT_COMMENTS}&include[]=replies&id=`;
+
+
+const URL_ICON = `${URL_BASE}/assets/pietsmiet/brand/icon.svg`;
+const URL_BANNER = `${URL_BASE}/assets/pietsmiet/brand/wordmark-plain-light-detail.svg`;
+
+const REGEX_VIDEO_URL = /https:\/\/www\.pietsmiet\.de\/videos\/(\d+)(.*)/; // /https:\/\/www\.pietsmiet\.de\/videos\/(.*)/;
+
+let HEADER_INTEGRITY = "9144f98d";
+
+const headerDict = {
+	'Content-Type': 'application/json',
+	'Accept': 'application/json',
+	'Access-Control-Allow-Headers': 'Content-Type',
+	'X-Origin-Integrity': HEADER_INTEGRITY
+};
+
 var config = {};
+
+function getPlatformId(id) {
+	return new PlatformID(PLATFORM, id.toString(), config.id);
+}
+function parseVideoId(url) {
+	const matches = REGEX_VIDEO_URL.exec(url);
+	return matches[1];
+}
+function parseThumbnailVariations(variationsDict) {
+	return new Thumbnails(variationsDict.map(y=>new Thumbnail(y.url, y.height)))
+}
+function parseAuthor(videoDict) {
+	return new PlatformAuthorLink(
+		getPlatformId(videoDict.id),
+		videoDict.channels[0].title,
+		URL_CHANNEL + videoDict.channels[0].url_slug,
+		URL_ICON
+	);
+}
+function parseDate(date) {
+	parseInt((new Date(date)).getTime() / 1000)
+}
 
 //Source Methods
 source.enable = function(conf, settings, savedState){
 	config = conf ?? {};
-	throw new ScriptException("This is a sample");
+	console.log("plugin enabled");
+	return "plugin enabled";
 }
+source.disable = function(conf, settings, savedState){
+	console.log("plugin disabled");
+	return "plugin disabled";
+}
+
 source.getHome = function() {
-	return new ContentPager([], false);
+	return new ContentPager(getHomeResults(0), true);
 };
+class HomePager extends VideoPager {
+	constructor(initialResults, hasMore) {
+		super(initialResults, hasMore);
+		this.page = 0;
+	}
+
+	nextPage() {
+		this.page++;
+		this.results = getHomeResults(this.page);
+		this.hasMore = true;
+		return this;
+	}
+}
+function getHomeResults(page) {
+	const url = URL_HOME + page;
+	const homeResp = http.GET(url, headerDict);
+	if(!homeResp.isOk)
+		throw new ScriptException(`Failed to get home from ${url} [${homeResp.code}]`)
+	const results = JSON.parse(homeResp.body).data;
+
+	return results.map(x=>new PlatformVideo({
+		id: getPlatformId(x.id),
+		name: x.title,
+		thumbnails: parseThumbnailVariations(x.thumbnail.variations),
+		author: parseAuthor(x),
+		datetime: parseDate(x.publish_date),
+		duration: x.duration,
+		// viewCount: 0, // todo: fix
+		url: URL_BASE + x.url,
+		shareUrl: x.short_url,
+		isLive: false
+	}))
+}
 
 source.searchSuggestions = function(query) {
 	throw new ScriptException("This is a sample");
@@ -52,19 +139,106 @@ source.getChannelContents = function(url) {
 
 //Video
 source.isContentDetailsUrl = function(url) {
-	throw new ScriptException("This is a sample");
+	return REGEX_VIDEO_URL.test(url);
 };
 source.getContentDetails = function(url) {
-	throw new ScriptException("This is a sample");
+	const video_id = parseVideoId(url);
+	const detailResponse = http.GET(URL_VIDEO_DETAILS + video_id, headerDict);
+	if(!detailResponse.isOk)
+		throw new ScriptException(`Failed to get detail for ${video_id} [${homeResp.code}]`)
+	const detailResults = JSON.parse(detailResponse.body);
+
+	const streamsResponse = http.GET(URL_VIDEO_PLAYER + video_id, headerDict);
+	if(!streamsResponse.isOk)
+		throw new ScriptException(`Failed to get streams for ${video_id} [${streamsResponse.code}]`)
+	const streamsResults = JSON.parse(streamsResponse.body);
+
+	let sourceVideos = [];
+
+	streamsResults.options.tracks.forEach(e => {
+		const hlssource = e.sources.hls;
+		sourceVideos.push(new HLSSource({
+			name: `${e.full_title} (HLS)`,
+			url: hlssource.src,
+			priority: false,
+			language: "German"
+		}));
+		const dashsource = e.sources.dash;
+		sourceVideos.push(new DashSource({
+			name: `${e.full_title} (Dash)`,
+			url: dashsource.src
+		}));
+	});
+	return new PlatformVideoDetails({
+		id: getPlatformId(detailResults.video.id), // streamsResults.tracks[0]
+		name: detailResults.video.title,
+		thumbnails: parseThumbnailVariations(detailResults.video.thumbnail.variations),
+		author: parseAuthor(detailResults.video),
+		uploadDate: parseDate(detailResults.video.publish_date),
+		duration: detailResults.video.duration,
+		// viewCount: detailResults.video.likes_count,
+		url: url,
+		isLive: false,
+		description: detailResults.video.description,
+		video: new VideoSourceDescriptor(sourceVideos), //See sources
+		live: null,
+		rating: new RatingLikes(detailResults.video.likes_count),
+		subtitles: []
+	});
+	
 };
+
+function getCommentResults(contextUrl, page) {
+	const video_id = parseVideoId(contextUrl);
+	const commentResponse = http.GET(`${URL_COMMENTS}${video_id}&page=${page}`, headerDict);
+	if(!commentResponse.isOk)
+		throw new ScriptException(`Failed to get comments for ${video_id} (page ${page}) [${commentResponse.code}]`)
+	const results = JSON.parse(commentResponse.body);
+
+	const comments = results.data?.map(i => {
+		const c = new Comment({
+			contextUrl: contextUrl,
+			author: new PlatformAuthorLink(getPlatformId(i.id),
+				i.user.name ?? "",
+				URL_PROFILE + i.user.url_slug,
+				i.user.avatar.variations[0]?.url ?? ""),
+			message: i.text ?? "",
+			date: parseDate(i.created_at),
+			replyCount: i.count_replies,
+			context: { commentId: i.id }
+		});
+
+		return c;
+	}) ?? [];
+
+	const hasMore = results.meta.current_page < result.result.last_page;
+	return new PietsmietDECommentPager(comments, hasMore,  contextUrl);
+
+}
+
+class PietsmietDECommentPager extends CommentPager {
+	constructor(comments, hasMore, contextUrl) {
+		super(comments, hasMore != null, contextUrl);
+		this.hasMore = hasMore;
+	}
+	nextPage() {
+		if(!this.hasMore)
+			return new CommentPager([], false);
+		this.page++;
+		return getCommentResults(this.contextUrl, this.page) ?? new CommentPager([], false);
+	}
+}
+
 
 //Comments
 source.getComments = function (url) {
-	throw new ScriptException("This is a sample");
+	// const video_id = parseVideoId(url);
+	return getCommentResults(id, url, 1, true);
 
-}
+
+};
 source.getSubComments = function (comment) {
 	throw new ScriptException("This is a sample");
-}
+};
 
 log("LOADED");

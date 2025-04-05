89a8e0c28e99cbf131a479be021c159fcce4701a
Bluscream <Bluscream@users.noreply.github.com>
2025-04-05 03:14:14 +0200

    Bump version to v45

--- build.ps1
+++ build.ps1
@@ -1,21 +1,41 @@
-param (
-    [int]$version
-)
+# param (
+#     [int]$version
+# )
 
 # Set Version in config
 $configPath = "build/config.json"
-$config = Get-Content -Raw -Path $configPath | ConvertFrom-Json
-$config.version = $version
-$config | ConvertTo-Json | Set-Content -Path $configPath
+$config = Get-Content -Path $configPath
+$json = $config | ConvertFrom-Json
+
+$oldVersion = $json.version
+$newVersion = $oldVersion + 1
+
+Write-Host "Old version: $oldVersion, bumping to $newVersion"
+
+$newVersionString = '"version": '+$newVersion+','
+Write-Host $newVersionString
+$config = $config -replace '"version": (\d+),', $newVersionString
+$config | Set-Content -Path $configPath
+
+# $config.version = $newVersion
+# $config | ConvertTo-Json | Set-Content -Path $configPath
 
 # build project
 npm run release:node
 
+# Wait for user to confirm release
+$confirmation = Read-Host "Do you want to proceed with the release? (yes/no)"
+if ($confirmation -ne "yes") {
+    Write-Host "Release aborted"
+    exit
+}
+
+
 # git stuff
 git add .
-git commit -m "Bump version to v$version"
+git commit -m "Bump version to v$newVersion"
 git push
 
 # make tag
-git tag $version -a -m "GrayJay PietSmiet.de Source Version $version"
+git tag $newVersion -a -m "GrayJay PietSmiet.de Source Version $newVersion"
 git push origin --tags

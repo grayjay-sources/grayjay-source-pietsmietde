463965ab6561de544883692030f7d28aaa8246fa
Bluscream <Bluscream@users.noreply.github.com>
2025-04-05 02:20:22 +0200

    Bump version to v37

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
-npm run release:node
+@REM npm run release:node
+git tag %1 -a -m "GrayJay PietSmiet.de Source Version %1"
+git add .
+git commit -m "Bump version to v%1"
+git push
+git push origin --tags

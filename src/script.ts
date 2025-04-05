8d536258a2381187f6c9a4a48be427603ca1e714
Bluscream <Bluscream@users.noreply.github.com>
2025-04-05 02:53:30 +0200

    Bump version to v44

--- src/script.ts
+++ src/script.ts
@@ -118,12 +118,12 @@ function init_source<
 
 //#region utils
 class Utils {
-	error = function (message: string, error: string | null | unknown) { // , _throw = false
+	error = function (message: string, error: string | null | unknown, _throw = false) {
 		utils.log(`${message}: ${error} (${JSON.stringify(error)})`, true); // const fmt = 
-		// if (_throw) {
+		if (_throw) {
 			// const log = errorLog; errorLog = "";
 			// throw new ScriptException(`${fmt}\n\n${log}`);
-		// }
+		}
 	}
 	log = function (message: string, toast = false) {
 		message = JSON.stringify(message);
